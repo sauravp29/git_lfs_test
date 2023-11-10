c2d67d9d6c8b5117bc271104d5af11dadd3514b2
sauravp29 <panchariyasaurav29@gmail.com>
2023-11-10 07:57:48 +0000

    3

--- combined_cibil_bank_django/client_api/input_files/ft_cash_pipeline/transformers/ft_cash_risk_bank_combined/process_bank_data_v3/dfs_feature_creation_feature_tools/py_scripts/utils/custom_exceptions_schema_creation.py
+++
@@ -1,4 +0,0 @@
-class SchemaDictSortingStageError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
--- combined_cibil_bank_django/client_api/live_scorecards/tests_data/test_transformer.py
+++
@@ -1,9 +0,0 @@
-from ml_implementation.ml_modules.ml_auto.encoding import Encoding
-import numpy as np
-
-
-def transform(raw_features_df):
-    """
-        mock test function
-    """
-    return raw_features_df
